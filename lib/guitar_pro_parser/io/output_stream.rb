a8d3d0446902e1d9f8011cb4b6c682fcfcabdad7
Alexander Borovykh <immaculate.pine@gmail.com>
2013-08-03 21:13:57 +0300

    Methods for output stream and tests for them.

--- lib/guitar_pro_parser/io/output_stream.rb
+++ lib/guitar_pro_parser/io/output_stream.rb
@@ -32,23 +32,23 @@ module GuitarProParser
     end
 
     def write_byte(*value)
-
+      write_to_buffer(value.pack('C'))
     end
 
     def write_signed_byte(*value)
-
+      write_to_buffer(value.pack('c'))
     end
     
-    def write_boolean(*value)
-      write_byte(value.zero? ? 0 : 1)
+    def write_boolean(value)
+      write_byte(value ? 1 : 0)
     end
 
-    def write_string(*value)
-
-    end
-
-    def write_bitmask(*value)
-
+    def write_bitmask(value)
+      binary = ''
+      value.reverse!
+      value.each { |bit| binary += bit ? '1' : '0'}
+      digit = binary.to_i(2)
+      write_byte(digit)
     end
 
     # Writes data chunk to file.
@@ -57,7 +57,7 @@ module GuitarProParser
     # 4 bytes - field length (including string length byte that follows this value)
     # 1 byte  - string length (N)
     # N bytes - string
-    def write_chunk(*value)
+    def write_chunk(value)
       write_integer(value.length + 1)
       write_byte(value.length)
       write_string(value)
@@ -65,6 +65,12 @@ module GuitarProParser
 
     def write_padding(size)
       size.times { write_byte(0) }
+    end
+
+    private
+
+    def write_string(value)
+      write_to_buffer(value)
     end
 
   end
--- spec/lib/guitar_pro_parser/io/output_stream_spec.rb
+++ spec/lib/guitar_pro_parser/io/output_stream_spec.rb
@@ -1,6 +1,91 @@
 require 'spec_helper'
 
+TEST_FILE_NAME = 'test_file'
+
 # TODO: Write tests
 describe GuitarProParser::OutputStream do
-  pending 'Write tests for all methods'
+
+  subject { GuitarProParser::OutputStream.new(TEST_FILE_NAME) }
+  let(:reader) { GuitarProParser::InputStream.new(TEST_FILE_NAME) }
+
+  describe '#write_integer' do
+    it 'writes integer' do
+      subject.write_integer 100
+      subject.close
+      reader.read_integer.should == 100
+    end
+  end
+
+  describe '#write_signed_integer' do
+    it 'writes signed integer' do
+      subject.write_signed_integer -100
+      subject.close
+      reader.read_signed_integer.should == -100
+    end
+  end
+
+  describe '#write_short_integer' do
+    it 'writes short integer' do
+      subject.write_short_integer 100
+      subject.close
+      reader.read_short_integer.should == 100
+    end
+  end
+
+  describe '#write_signed_short_integer' do
+    it 'writes signed short integer' do
+      subject.write_signed_short_integer -100
+      subject.close
+      reader.read_signed_short_integer.should == -100
+    end
+  end
+
+  describe '#write_byte' do
+    it 'writes byte' do
+      subject.write_byte 100
+      subject.close
+      reader.read_byte.should == 100
+    end
+  end
+
+  describe '#write_signed_byte' do
+    it 'writes signed byte' do
+      subject.write_signed_byte -100
+      subject.close
+      reader.read_signed_byte.should == -100
+    end
+  end
+
+  describe '#write_boolean' do
+    it 'writes true' do
+      subject.write_boolean true
+      subject.close
+      reader.read_boolean.should == true
+    end
+
+    it 'writes false' do
+      subject.write_boolean false
+      subject.close
+      reader.read_boolean.should == false
+    end
+  end
+
+  describe '#write_chunk' do
+    it 'writes chunk' do
+      test_string = 'Test string'
+      subject.write_chunk(test_string)
+      subject.close
+      reader.read_chunk.should == test_string
+    end
+  end
+
+  describe '#write_padding' do
+    it 'writes empty bytes' do
+      subject.write_padding(4)
+      subject.close
+      reader.read_integer.should == 0
+    end
+  end
+
+  after(:all) { system("rm #{TEST_FILE_NAME}") }
 end
